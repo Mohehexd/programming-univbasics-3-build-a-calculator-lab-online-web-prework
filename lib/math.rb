98cf445e476023267f4e9ca1c0e2a39732142976
Guillermo Martinez <dumblemoe@gmail.com>
2019-08-07 00:35:20 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -2,18 +2,22 @@ def addition(num1, num2)
   num1+num2
 end
 addition(1,2)
+
 def subtraction(num1, num2)
   num1-num2
 end
 subtraction(1,2)
+
 def division(num1, num2)
   num1/num2
 end
 division(1,2)
+
 def multiplication(num1, num2)
   num1*num2
 end
-multiplication(1,2)
+multiplication(2,2)
+
 def modulo(num1, num2)
   Math.sin(num1,num2)
 end
