c7c3dfab0fd7104b8ae24b23675a41fcb8613fde
Guillermo Martinez <dumblemoe@gmail.com>
2019-08-07 00:28:28 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -3,13 +3,13 @@ def addition(num1, num2)
 end
 addition(1,2)
 def subtraction(num1, num2)
-
+  num1-num2
 end
-
+subtraction(1,2)
 def division(num1, num2)
-
+num1/num2
 end
-
+division(1,2)
 def multiplication(num1, num2)
 
 end
