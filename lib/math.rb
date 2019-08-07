23feb2024822d7f65f1703417fb2b2eccb871550
Guillermo Martinez <dumblemoe@gmail.com>
2019-08-07 00:34:18 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -7,17 +7,18 @@ def subtraction(num1, num2)
 end
 subtraction(1,2)
 def division(num1, num2)
-num1/num2
+  num1/num2
 end
 division(1,2)
 def multiplication(num1, num2)
-
+  num1*num2
 end
-
+multiplication(1,2)
 def modulo(num1, num2)
-
+  Math.sin(num1,num2)
 end
-
+modulo(1,2)
 def square_root(num)
-
+  Math.sqrt(num)
 end
+square_root(2)  
